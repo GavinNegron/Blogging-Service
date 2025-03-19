23b737c9bcfa5fa031a18509552e79e27fc81005
GavinNegron <gavinnegron@icloud.com>
2025-03-19 16:31:34 -0400

    Update register styles

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,36 +7,80 @@ import './register.sass'
 export default async function Register() {
   return (
     <div className='register-page'>
-      <NavbarLogin/>
-      <main className='d-flex ai-center jc-center flex-col'>
-        <div className="register">
+      <NavbarLogin />
+      <main className='d-flex ai-center jc-center flex-row'>
+        <div className="register-img">
+          <img src="/register.svg" alt="secure registration" />
+        </div>
+        <section className="register">
           <div className="register__inner">
             <div className="register__section register__section--form">
-              <div className="register__header">
+              <header className="register__header">
                 <h1>Create Account</h1>
-              </div>
-              <div className="register__form">
-                <span>Enter email:</span>
-                <input type="email" />
-                <span>Enter password:</span>
-                <input type="password" />
-                <div className="register__button">
-                  <button onClick={signUp}>Submit</button>
+              </header>
+              <form className="register__form" onSubmit={signUp}>
+                <div className="register__form-row">
+                  <div className="register__form-item">
+                    <label htmlFor="firstName">First Name:</label>
+                    <input type="text" id="firstName" name="firstName" required />
+                  </div>
+                  <div className="register__form-item">
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input type="text" id="lastName" name="lastName" required />
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="register__section register__section--content">
-              <div className="register__section--content__header">
-                <h1>Already have an account?</h1>
-              </div>
-              <div className="register__section--content__button">
-                <button>
-                  <Link href='/register'>Login Now</Link>
-                </button>
-              </div>
+                <div className="register__form-row">
+                  <div className="register__form-item">
+                    <label>Select Gender:</label>
+                    <div className="register__form-row">
+                      <div className="register__form-item--row">
+                        <input id='gender-male' name="gender" type="radio" value='Male' required />
+                        <label htmlFor='gender-male'>Male</label>
+                      </div>
+                      <div className="register__form-item--row">
+                        <input id='gender-female' name="gender" type="radio" value='Female' required />
+                        <label htmlFor='gender-female'>Female</label>
+                      </div>
+                      <div className="register__form-item--row">
+                        <input id='gender-pns' name="gender" type="radio" value='pns' required />
+                        <label htmlFor='gender-pns'>Prefer not to say</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="register__form-row">
+                  <div className="register__form-item">
+                    <label htmlFor="email">Email Address:</label>
+                    <input type="email" id="email" name="email" required />
+                  </div>
+                </div>
+                <div className="register__form-row">
+                  <div className="register__form-item">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" required />
+                  </div>
+                  <div className="register__form-item">
+                    <label htmlFor="password-confirm">Confirm Password:</label>
+                    <input type="password" name="password-confirm" id="password-confirm" required />
+                  </div>
+                </div>
+                <div className="register__form-row">
+                  <div className="register__form-item">
+                    <label htmlFor="phoneNumber">Phone Number:</label>
+                    <input type="tel" id="phoneNumber" name="phoneNumber" required />
+                  </div>
+                  <div className="register__form-item">
+                    <label htmlFor="birthDate">Birth Date:</label>
+                    <input type="date" id="birthDate" name="birthDate" required />
+                  </div>
+                </div>
+                <div className="register__button">
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
             </div>
           </div>
-        </div>
+        </section>
       </main>
     </div>
   );
